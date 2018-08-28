6180c5535a3d8bc956f9737b678efba22cb46c6c
Matt <voorhees.matthew@gmail.com>
2018-08-28 10:24:05 -0700

    Add board class skeleton

---
+++ Chess/Board.rb
@@ -0,0 +1,40 @@
+class Board 
+  def initialize
+    @rows = rows 
+    # @sentinel = sentinel
+  end 
+  
+  def [](pos) 
+  end 
+  
+  def []=(pos, val) 
+  end 
+  
+  def move_piece(color, start_pos, end_pos)
+  end 
+  
+  def valid_pos?(pos) 
+  end 
+  
+  def add_piece(piece, pos)
+  end 
+  
+  def checkmate?(color) 
+  end 
+  
+  def in_check?(color) 
+  end 
+  
+  def find_king(color) 
+  end 
+  
+  def pieces
+  end 
+  
+  def dup 
+  end 
+  
+  def move_piece!(color, start_pos, end_pos)
+  end 
+
+end 
