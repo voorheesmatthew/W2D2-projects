41f7bcefa3692b8b30a062166bd4e6e7bfe5ffae
Matt <voorhees.matthew@gmail.com>
2018-08-28 10:37:02 -0700

    Add piece class skeleton

---
+++ Chess/slideable_piece.rb
@@ -0,0 +1,35 @@
+class Rook < Piece 
+  def symbol
+    
+  end
+  
+  protected
+  
+  def move_dirs
+  end
+  
+end
+
+class Bishop < Piece 
+  def symbol
+    
+  end
+  
+  protected
+  
+  def move_dirs
+  end
+  
+end
+
+class Queen < Piece 
+  def symbol
+    
+  end
+  
+  protected
+  
+  def move_dirs
+  end
+  
+end
