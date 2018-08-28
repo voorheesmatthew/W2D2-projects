6dbfe90ab423bd74f7e5b0f8c5cc2ba8996b2286
Matt <voorhees.matthew@gmail.com>
2018-08-28 10:40:59 -0700

    Add skeleton for steppable and Pawn classes

---
+++ Chess/pawn.rb
@@ -0,0 +1,29 @@
+class Pawn
+  
+  def symbol
+    
+  end
+  
+  def move_dirs
+    
+  end
+  
+  private
+  
+  def at_start_row?
+    
+  end
+  
+  def forward_dir
+    
+  end
+  
+  def forward_steps
+    
+  end
+  
+  def side_attacks
+    
+  end
+  
+end
---
+++ Chess/stepable.rb
@@ -0,0 +1,25 @@
+class Knight < Piece
+  
+  def symbol
+    
+  end
+  
+  protected
+  
+  def move_dirs
+  end
+  
+end
+
+class King < Piece
+  
+  def symbol
+    
+  end
+  
+  protected
+  
+  def move_dirs
+  end
+  
+end
