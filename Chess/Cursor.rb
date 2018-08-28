694e63c2eae15fd1c84d8d09a31e21889b1ec82b
Matt <voorhees.matthew@gmail.com>
2018-08-28 10:20:42 -0700

    Add skeleton for display and cursor classes

---
+++ Chess/Cursor.rb
@@ -0,0 +1,23 @@
+class Cursor
+  def initialize(cursor_pos, board)
+    @board = board 
+    @cursor_pos = cursor_pos
+    @selected = selected
+  end 
+  
+  def get_input
+  end 
+  
+  def toggle_selected 
+  end 
+  
+  private 
+  def handle_key(key)
+  end 
+  
+  def read_char
+  end 
+  
+  def update_pos(diff)
+  end 
+end 
---
+++ Chess/Display.rb
@@ -0,0 +1,9 @@
+class Display 
+  def initialize
+    @board = board 
+    @cursor = cursor
+  end 
+  
+  def render 
+  end 
+end 
