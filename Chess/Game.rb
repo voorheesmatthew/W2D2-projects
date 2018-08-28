2f09e562f9dcfbc0f844339063fbba34fb751fc6
Matt <voorhees.matthew@gmail.com>
2018-08-28 10:18:07 -0700

    Add game class skeleton

---
+++ Chess/Game.rb
@@ -0,0 +1,18 @@
+class Game
+  def initialize
+    @board = board 
+    @display = display 
+    @players = players
+    @current_player = current_player
+  end
+  
+  def play 
+  end 
+  
+  private 
+  def notify_players 
+  end 
+  
+  def swap_turn!
+  end 
+end 
